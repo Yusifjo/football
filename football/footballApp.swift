0713f40ead004586c6e1c9526e2f77b2dedefc54
Yusifjo <yibrahimov86@gmail.com>
2026-09-16 18:37:32 +0200

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Joseph ibrahimov on 16.09.26.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
